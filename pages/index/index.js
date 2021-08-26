483f84831da7ef5b28ce0321f4ee356c15b7247d
William Barbosa <ingeniero.wb@gmail.com>
2021-08-26 12:40:11 -0500

    KETCLOAJ QUE NO REDIRECCIONE

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,71 +1,109 @@
-//Iniciamos el documento
-$(document).ready(function () {
-    //Verifico si el token exite en el cliente y verifico que el token este activo en el servidor                
-    var token_actual = getLocalStorage(name_local_storage);
-
-    //Verifico si el token esta vacio, para enviarlo a que ingrese de nuevo
-    if ($.isEmptyObject(token_actual)) {
-        location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
+//Array del consumo con el back
+keycloak.init(initOptions).then(function (authenticated) {
+    //Si no esta autenticado lo obliga a ingresar al keycloak
+    if (authenticated === false)
+    {
+        keycloak.login();
     } else
     {
+        //Guardamos el token en el local storage
+        if (typeof keycloak === 'object') {
 
-        //Realizo la peticion para cargar el formulario
-        $.ajax({
-            type: 'POST',
-            data: {"token": token_actual.token},
-            url: url_pv + 'Session/consultar_usuario'
-        }).done(function (data) {
-            if (data == 'error_metodo')
-            {
-                notify("danger", "ok", "Convocatorias:", "Se registro un error en el método, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
-            } 
-            else
-            {
-                if (data == 'error_token')
+            var token_actual = JSON.parse(JSON.stringify(keycloak));
+
+            //Verifica si el token actual tiene acceso de lectura
+            permiso_lectura_keycloak(token_actual.token, "SICON-USUARIOS");
+            
+            //Cargamos el menu principal
+            $.ajax({
+                type: 'POST',
+                data: {"token": token_actual.token, "id": getURLParameter('id'), "m": getURLParameter('m'), "p": getURLParameter('p'), "sub": getURLParameter('sub')},
+                url: url_pv + 'Administrador/menu'
+            }).done(function (result) {
+                if (result == 'error_token')
                 {
                     location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
                 } else
                 {
-                    if (data == 'error')
+                    $("#menu_principal").html(result);
+                }
+            });
+
+
+            //Cargar por defecto los manuales de funcionario
+            $(".manuales_externos").css("display", "none");
+            $(".manuales_internos").css("display", "block");
+            
+            /*
+            if (json.externo == "Si")
+            {
+                $(".manuales_externos").css("display", "block");
+                $(".manuales_internos").css("display", "none");
+            } else
+            {
+                $(".manuales_externos").css("display", "none");
+                $(".manuales_internos").css("display", "block");
+
+            }
+            */
+
+            /*Se inactiva, para despues colocar los nombres de los datos del keycloak
+            $.ajax({
+                type: 'POST',
+                data: {"token": token_actual.token},
+                url: url_pv + 'Session/consultar_usuario'
+            }).done(function (data) {
+                if (data == 'error_metodo')
+                {
+                    notify("danger", "ok", "Convocatorias:", "Se registro un error en el método, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
+                } else
+                {
+                    if (data == 'error_token')
                     {
-                        location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
-                    } 
-                    else
+                        //location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
+                    } else
                     {
-                        var json = JSON.parse(data);
-                        if(json.user.primer_nombre==null)
+                        if (data == 'error')
                         {
-                           json.user.primer_nombre="";
-                        }
-                        if(json.user.segundo_nombre==null)
+                            location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
+                        } else
                         {
-                           json.user.segundo_nombre="";
-                        }
-                        if(json.user.primer_apellido==null)
-                        {
-                           json.user.primer_apellido="";
-                        }
-                        if(json.user.segundo_apellido==null)
-                        {
-                           json.user.segundo_apellido="";
-                        }
-                        $("#nombre_bienvenida").html(json.user.primer_nombre+" "+json.user.segundo_nombre+" "+json.user.primer_apellido+" "+json.user.segundo_apellido);                        
-                        
-                        if(json.externo=="Si")
-                        {                            
-                            $(".manuales_externos").css("display","block");
-                            $(".manuales_internos").css("display","none");
-                        }
-                        else
-                        {
-                            $(".manuales_externos").css("display","none");
-                            $(".manuales_internos").css("display","block");
-                            
+                            var json = JSON.parse(data);
+                            if (json.user.primer_nombre == null)
+                            {
+                                json.user.primer_nombre = "";
+                            }
+                            if (json.user.segundo_nombre == null)
+                            {
+                                json.user.segundo_nombre = "";
+                            }
+                            if (json.user.primer_apellido == null)
+                            {
+                                json.user.primer_apellido = "";
+                            }
+                            if (json.user.segundo_apellido == null)
+                            {
+                                json.user.segundo_apellido = "";
+                            }
+                            $("#nombre_bienvenida").html(json.user.primer_nombre + " " + json.user.segundo_nombre + " " + json.user.primer_apellido + " " + json.user.segundo_apellido);
+
+                            if (json.externo == "Si")
+                            {
+                                $(".manuales_externos").css("display", "block");
+                                $(".manuales_internos").css("display", "none");
+                            } else
+                            {
+                                $(".manuales_externos").css("display", "none");
+                                $(".manuales_internos").css("display", "block");
+
+                            }
                         }
                     }
                 }
-            }
-        });
-
+            });
+            */
+        }
     }
-});
+}).catch(function () {
+    location.href = url_pv_admin + 'error_keycloak.html';
+});    
