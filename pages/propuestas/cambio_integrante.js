39a8f0b5c72ec6f9292970636ead0b85c6b3c9d4
William Barbosa <ingeniero.wb@gmail.com>
2022-01-28 15:24:41 -0500

    Se finaliza formulario de cambio de integrante y cargue de documentacion adm

--- pages/propuestas/cambio_integrante.js
+++ pages/propuestas/cambio_integrante.js
@@ -39,12 +39,6 @@ $(document).ready(function () {
                                 if (data === 'ingresar')
                                 {
 
-                                    //Asignamos el valor a input conv
-                                    $("#propuesta").attr('value', getURLParameter('id'));
-
-                                    //disabled todos los componentes
-                                    //$("#formulario_principal input,textarea,select,button[type=submit]").attr("disabled", "disabled");
-
                                     //Verifica si el token actual tiene acceso de lectura
                                     permiso_lectura(token_actual, "Menu Participante");
 
@@ -302,7 +296,7 @@ $(document).ready(function () {
                                     //Realizo la peticion para cargar el formulario
                                     $.ajax({
                                         type: 'GET',
-                                        data: {"token": token_actual.token, "conv": $("#conv").attr('value'), "modulo": "Menu Participante", "m": getURLParameter('perfil'), "p": getURLParameter('p')},
+                                        data: {"token": token_actual.token, "conv": getURLParameter('id'), "modulo": "Menu Participante", "m": getURLParameter('perfil'), "p": getURLParameter('p')},
                                         url: url_pv + 'Personasnaturales/formulario_integrante'
                                     }).done(function (data) {
                                         if (data == 'error_metodo')
@@ -471,16 +465,117 @@ $(document).ready(function () {
                                     });
                                     
                                     $("#cambio-btn-si").click(function () {
-                                        var html_table = '<ul>';
-                                        alert(typeof parseInt($("#id").val()));
-                                        if (parseInt($("#id").val())<=0) {
-                                            alert("no existe");
-                                        }
-                                        
-                                            
-                                        
-                                        html_table = html_table + '</ul>';
-                                        notify("danger", "remove", "Cambio de integrante:", "<p>Los siguientes requisitos son obligatorios</p>" + html_table);
+                                        //Realizo la peticion para cargar el formulario
+                                        $.ajax({
+                                            type: 'GET',
+                                            data: {"token": token_actual.token, conv: getURLParameter('id'), modulo: "Menu Participante", m: getURLParameter('perfil'), propuesta: $("#propuesta").attr('value'),id_participante_reemplazo:$("#id_participante_reemplazo").attr('value')},
+                                            url: url_pv + 'PropuestasDocumentacion/validar_requisitos_cambio_integrante'
+                                        }).done(function (data) {
+                                            if (data == 'error_metodo')
+                                            {
+                                                notify("danger", "ok", "Convocatorias:", "Se registro un error en el método, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
+                                            } else
+                                            {
+                                                if (data == 'error_token')
+                                                {
+                                                    location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
+                                                } else
+                                                {
+                                                    if (data == 'crear_propuesta')
+                                                    {
+                                                        location.href = url_pv_admin + 'pages/propuestas/propuestas_busqueda_convocatorias.html?msg=Para poder inscribir la propuesta debe crear el perfil de agrupacion.&msg_tipo=danger';
+                                                    } else
+                                                    {
+                                                        if (data == 'acceso_denegado')
+                                                        {
+                                                            notify("danger", "remove", "Convocatorias:", "No tiene permisos para ver la información.");
+                                                        } else
+                                                        {
+                                                            var json = JSON.parse(data);
+
+                                                            var count = Object.keys(json).length;
+
+                                                            if (count > 0)
+                                                            {
+                                                                var html_table = '<ul>';
+                                                                if(json.documentos_administrativos===0)
+                                                                {
+                                                                    html_table = html_table + '<li>No ha cargado la documentación administrativa del nuevo integrante.</li>';
+                                                                }
+                                                                if(json.integrante_nuevo===0)
+                                                                {
+                                                                    html_table = html_table + '<li>No ha ingresado la información del nuevo integrante.</li>';
+                                                                }
+                                                                html_table = html_table + '</ul>';
+                                                                notify("danger", "remove", "Convocatorias:", "<p>Los siguientes requisitos son obligatorios</p>" + html_table);
+                                                            } else
+                                                            {
+                                                                //VALIDAR EL AJUSTE DEL ESTADO DEL CAMBIO DE INTEGRANTE
+                                                                $.ajax({
+                                                                    type: 'POST',
+                                                                    url: url_pv + 'Personasnaturales/editar_participante',
+                                                                    data: "id="+$("#id_participante_reemplazo").attr('value')+"&conv="+getURLParameter('id')+"&propuesta="+getURLParameter('p')+"&estado_cambio_integrante=49&modulo=Menu Participante&token=" + token_actual.token
+                                                                }).done(function (result) {
+                                                                    var result = result.trim();
+
+                                                                    if (result == 'error')
+                                                                    {
+                                                                        notify("danger", "ok", "Integrantes:", "Se registro un error, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
+                                                                    } else
+                                                                    {
+                                                                        if (result == 'error_token')
+                                                                        {
+                                                                            location.href = url_pv_admin + 'index.html?msg=Su sesión ha expirado, por favor vuelva a ingresar.&msg_tipo=danger';
+                                                                        } else
+                                                                        {
+                                                                            if (result == 'acceso_denegado')
+                                                                            {
+                                                                                notify("danger", "remove", "Usuario:", "No tiene permisos para editar información.");
+                                                                            } else
+                                                                            {
+                                                                                if (result == 'error_metodo')
+                                                                                {
+                                                                                    notify("danger", "ok", "Integrantes:", "Se registro un error, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
+                                                                                } else
+                                                                                {
+                                                                                    if (result == 'error_representante')
+                                                                                    {
+                                                                                        notify("danger", "ok", "Integrantes:", "No puede registrar mas de un representante.");
+                                                                                    } else
+                                                                                    {
+                                                                                        if (result == 'error_representante_suplente')
+                                                                                        {
+                                                                                            notify("danger", "ok", "Integrantes:", "No puede registrar mas de un representante suplente.");
+                                                                                        } else
+                                                                                        {
+                                                                                            if (result == 'error_participacion')
+                                                                                            {
+                                                                                                notify("danger", "ok", "Convocatorias:", "No puede registrar este integrante, debido a que su número de documento ya esta en proceso de inscripción en esta convocatoria con otra propuesta, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co para mayor información.");
+                                                                                            } 
+                                                                                            else
+                                                                                            {
+                                                                                                if (isNaN(result)) {
+                                                                                                    notify("danger", "ok", "Integrantes:", "Se registro un error, comuníquese con la mesa de ayuda convocatorias@scrd.gov.co");
+                                                                                                } else
+                                                                                                {
+                                                                                                    notify("success", "ok", "Integrantes:", "Se Guardó con el éxito la solicitud para el nuevo integrante.");
+                                                                                                }
+                                                                                            }
+                                                                                        }                                                                                                            
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+
+                                                                });
+                                                            }
+                                                        }
+                                                    }
+
+                                                }
+                                            }
+                                        });    
                                     });
                                     
                                     //Valido formulario
@@ -1204,6 +1299,7 @@ function cargar_formulario(token_actual)
                                     {
                                         var json = JSON.parse(data);
 
+                                        $("#tabla_administrativos").html('');
                                         
                                         var html_table = '';
                                         $.each(json.administrativos, function (key2, documento) {
@@ -1258,7 +1354,7 @@ function cargar_tabla_archivos(token_actual, documento,estado) {
     //Realizo la peticion para cargar el formulario
     $.ajax({
         type: 'GET',
-        data: {documento: documento, "token": token_actual.token, conv: $("#conv").attr('value'), modulo: "Menu Participante", m: getURLParameter('m'), propuesta: $("#propuesta").attr('value')},
+        data: {documento: documento, "token": token_actual.token, conv: getURLParameter('id'), modulo: "Menu Participante", m: getURLParameter('perfil'), propuesta: $("#propuesta").attr('value')},
         url: url_pv + 'PropuestasDocumentacion/buscar_archivos'
     }).done(function (data) {
         if (data == 'error_metodo')
@@ -1310,7 +1406,7 @@ function cargar_tabla_link(token_actual, documento, estado) {
     //Realizo la peticion para cargar el formulario
     $.ajax({
         type: 'GET',
-        data: {documento: documento, "token": token_actual.token, conv: $("#conv").attr('value'), modulo: "Menu Participante", m: getURLParameter('m'), propuesta: $("#propuesta").attr('value')},
+        data: {documento: documento, "token": token_actual.token, conv: getURLParameter('id'), modulo: "Menu Participante", m: getURLParameter('perfil'), propuesta: $("#propuesta").attr('value')},
         url: url_pv + 'PropuestasDocumentacion/buscar_link'
     }).done(function (data) {
         if (data == 'error_metodo')
