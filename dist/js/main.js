19374645ddf649225c42a2f4952c266f4a7f76af
wilmer.mogollon <wilmer.mogollon@scrd.gov.co>
2022-04-01 09:03:34 -0500

    Rediseño jurados

--- dist/js/main.js
+++ dist/js/main.js
@@ -289,7 +289,7 @@ $(document).ready(function () {
         //Cargamos el menu principal
         $.ajax({
             type: 'POST',
-            data: {"token": token_actual.token, "id": getURLParameter('id'), "m": getURLParameter('m'), "p": getURLParameter('p'), "sub": getURLParameter('sub'), "perfil": getURLParameter('perfil')},
+            data: {"token": token_actual.token, "id": getURLParameter('id'), "m": getURLParameter('m'), "p": getURLParameter('p'), "sub": getURLParameter('sub'), "perfil": getURLParameter('perfil'), "opcion_menu":$("#opcion_menu").val()},
             url: url_pv + 'Administrador/menu_participante'
         }).done(function (result) {
             if (result == 'error_token')
