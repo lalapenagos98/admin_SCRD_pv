18f92aa5a5d15fb5b70cf39be97c920e1a4505de
lalapenagos98 <lalitapenagos98@hotmail.com>
2023-08-02 22:32:10 -0500

    Se crea la columna de fecha y hora de creacion

--- pages/jurados/mentoresseleccion.js
+++ pages/jurados/mentoresseleccion.js
@@ -391,7 +391,7 @@ function cargar_tabla(token_actual) {
             //$(".check_activar_f").removeAttr("checked");
             acciones_registro(token_actual);
             //validator_form(token_actual);
-
+            
         },
         "rowCallback": function (row, data, index) {
 
@@ -421,6 +421,11 @@ function cargar_tabla(token_actual) {
             {"data": "Apellidos",
                 render: function (data, type, row) {
                     return row.apellidos;
+                },
+            },
+            {"data": "Fecha de creación",
+                render: function (data, type, row) {
+                    return row.fecha_creacion;
                 },
             },
             {"data": "Puntaje",
