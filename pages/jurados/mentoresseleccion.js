a6b2d42254c25714b0898e391c21179d337bfcd4
lalapenagos98 <lalitapenagos98@hotmail.com>
2023-08-15 11:42:55 -0500

    Se subrayan de color verde los mentores con puntaje mayor a 80

--- pages/jurados/mentoresseleccion.js
+++ pages/jurados/mentoresseleccion.js
@@ -396,6 +396,14 @@ function cargar_tabla(token_actual) {
         "rowCallback": function (row, data, index) {
 
         },
+        "rowCallback": function (row, data, index) {
+            // Verificar si el puntaje es mayor a 80 cambia de color a verde
+            if (data["puntaje"]>=80) {
+                $('td', row).css('background-color', '#dcf4dc');
+            } else if (!data["puntaje"]) {
+                $('td', row).css('background-color', '');
+            }
+        },
         "columns": [
             {"data": "Cod. de inscripción",
                 render: function (data, type, row) {
